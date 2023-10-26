d0b07bf6bf331a74514cdeaf46235985121879ac
Pranav014 <pranav.k1998@gmail.com>
2023-10-25 20:45:52 -0400

    packer with user

---
+++ packer_tmp/variables.auto.pkrvar.hcl
@@ -0,0 +1,6 @@
+source_ami = "ami-06db4d78cb1d3bbf9"
+subnet_id = "subnet-0ef821d9bbfe01892"
+instanceType = "t2.micro"
+aws_region="us-east-1"
+ssh_username="admin"
+ami_users=["956043594788"]
--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "---x---Installing Java 17---x---"
 sudo apt install openjdk-17-jdk -y
 
 #sudo apt install unzip
-export CSV_PATH="/opt/users.csv"
+echo "export CSV_PATH=\"/opt/users.csv\""
 
 echo "---x---Setting Java Environment Variables---x---"
 echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/" >> ~/.bashrc
@@ -19,24 +19,22 @@ source ~/.bashrc
 echo "---x---Installing Maven---x---"
 sudo apt install maven -y
 
-echo "---x---Installing PostgreSQL---x---"
-sudo apt install postgresql postgresql-contrib -y
-
-# Start and enable PostgreSQL to start on boot
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
-
-#!/bin/bash
+#echo "---x---Installing PostgreSQL---x---"
+#sudo apt install postgresql postgresql-contrib -y
+#
+## Start and enable PostgreSQL to start on boot
+#sudo systemctl start postgresql
+#sudo systemctl enable postgresql
 
 # Create a PostgreSQL database
 
-export CSV_PATH=/opt/users.csv
+#export CSV_PATH=/opt/users.csv
 
-# Configure PostgreSQL: set password, create database, and create user
-sudo -u postgres createuser  myuser  # --noninteractive --pwprompt
- sudo -u postgres psql -c "ALTER USER myuser WITH PASSWORD 'mypassword';"
-#sudo -u postgres createdb postgres
-sudo -u postgres psql -c "ALTER USER myuser WITH SUPERUSER"
+## Configure PostgreSQL: set password, create database, and create user
+#sudo -u postgres createuser  myuser  # --noninteractive --pwprompt
+# sudo -u postgres psql -c "ALTER USER myuser WITH PASSWORD 'mypassword';"
+##sudo -u postgres createdb postgres
+#sudo -u postgres psql -c "ALTER USER myuser WITH SUPERUSER"
 
 
 
