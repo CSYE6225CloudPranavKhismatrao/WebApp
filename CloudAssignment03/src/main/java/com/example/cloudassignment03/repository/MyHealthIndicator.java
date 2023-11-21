30f5d040e8a614d2b0e578ffefa72cded81c4411
Pranav Khismatrao <44774105+Pranav014@users.noreply.github.com>
2023-11-21 07:17:30 -0500

    Feature assignment09 (#50)
    * Assignment 7
    
    * Assignment 7 - json logs
    
    * Assignment 7 - json logs test
    
    * Assignment 7 - logging config
    
    * Assignment 7 - logging config
    
    * Assignment 7 - logging config
    
    * Assignment 7 - logging config
    
    * Assignment 7 - logging config
    
    * Assignment 7 - logging config
    
    * actuator config

--- CloudAssignment03/src/main/java/com/example/cloudassignment03/repository/MyHealthIndicator.java
+++ CloudAssignment03/src/main/java/com/example/cloudassignment03/repository/MyHealthIndicator.java
@@ -1,36 +1,25 @@
 package com.example.cloudassignment03.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
-@Component
-public class MyHealthIndicator implements HealthIndicator {
-    private final DataSource dataSource;
-
-    public MyHealthIndicator(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    private boolean checkServiceStatus() {
-        try {
-            return dataSource.getConnection() != null;
-        } catch (SQLException e) {
-            return false;
-        }
-
-    }
-
-    @Override
-    public Health health() {
-        boolean isServiceHealthy = checkServiceStatus(); // Replace with your logic
-        if (isServiceHealthy) {
-            return Health.up().build();
-        } else {
-            return Health.down().build();
-        }
-    }
-}
+//@Component
+//public class MyHealthIndicator extends DataSourceHealthIndicator {
+//
+//
+//    @Override
+//    protected void doHealthCheck(Health.Builder builder) throws Exception {
+//        super.doHealthCheck(builder);
+//        builder.up().withDetail("message", "Database is up");
+//    }
+//
+//
+//}
